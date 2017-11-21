daddc9e185266c095dde1d2263f7e6fb105f4d5d
fallon <fallon@fallon>
2017-11-20 22:14:27 -0500

    My next gripe

--- src/packt/jee/eclipse/git/test/HelloGit.java
+++ src/packt/jee/eclipse/git/test/HelloGit.java
@@ -4,5 +4,8 @@ public class HelloGit {
 	public static void main(String[] args){
 		System.out.println("Up yours Alexia Athienitis.");
 		System.out.println("You too Prof. Daniels");
+		System.out.println("Screw you. Alexia, for never "
+				+ "\ngiving me back the points I deserved"
+				+ "\n for my homework that wasn't actually late.");
 	}
 }
